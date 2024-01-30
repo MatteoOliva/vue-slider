7eae818eb7f6b791f459de9b5b67aedc6d58f0a9
MatteoOliva <oliva.matteo98@outlook.com>
2024-01-30 16:03:04 +0100

    feat: inizio bonus 1

--- js/script.js
+++ js/script.js
@@ -4,6 +4,8 @@ createApp({
     data() {
         return {
             activeImage: 0,
+            
+            
             images: [
                 'img/01.webp', 'img/02.webp', 'img/03.webp', 'img/04.webp', 'img/05.webp',
             ],
