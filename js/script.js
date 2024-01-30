89a9d0ef5b90d15091314ec4f4ac79a998489444
MatteoOliva <oliva.matteo98@outlook.com>
2024-01-30 14:45:51 +0100

    Bloccato: non riesco a andare dall ultima immagine alla 0

--- js/script.js
+++ js/script.js
@@ -7,7 +7,17 @@ createApp({
             images: [
                 'img/01.webp', 'img/02.webp', 'img/03.webp', 'img/04.webp', 'img/05.webp',
             ],
-            
+
         };
     },
+    methods: {
+        nextClick() {
+            if (this.activeImage > this.images.lenght -2) {
+                this.activeImage = 0;
+            } else {
+                this.activeImage++;
+                
+            }
+        }
+    }
 }).mount('#app')
